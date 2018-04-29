543157e354fb8c8863532e81a4e24301b9a6d306
borsukvasyl <v.borsuk2000@gmail.com>
2018-04-29 22:23:50 +0300

    renamed builtins

--- server/Server.cpp
+++ server/Server.cpp
@@ -8,13 +8,13 @@ namespace ip = boost::asio::ip;
 Server::Server(asio::io_service &io_service, unsigned short port):
         tcp_acceptor(io_service, ip::tcp::endpoint(ip::tcp::v4(), port)) {
     const std::map<std::string, myshell::Executor*> builtins = {
-            {"merrno",   new myshell::Merrno(interpreter)},
-            {"mpwd",     new myshell::Mpwd()},
-            {"mcd",      new myshell::Mcd()},
-            {"mexit",    new myshell::Mexit()},
-            {".",        new myshell::FileInterpreter(interpreter)},
-            {"mecho",    new myshell::Mecho()},
-            {"mexport",  new myshell::Mexport()}
+            {"errno",   new myshell::Merrno(interpreter)},
+            {"pwd",     new myshell::Mpwd()},
+            {"cd",      new myshell::Mcd()},
+            {"exit",    new myshell::Mexit()},
+            {".",       new myshell::FileInterpreter(interpreter)},
+            {"echo",    new myshell::Mecho()},
+            {"export",  new myshell::Mexport()}
     };
     interpreter.set_builtins(builtins);
     start_accept();
